278f27bfb0874d14bb95ec1be332bd12ef4c0627
Tolu Bada <142679079+DaRealBada@users.noreply.github.com>
2024-10-20 17:20:28 +0100

    Add files via upload

---
+++ App.js
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import './App.css';
+import Login  from "./Components/Login/Login";
+import Home from "./Components/Home/Home";    // Adjust the path as needed
+import Register from './Components/Login/Register';
+import Topics from './Components/Topics/Topics';
+import { Tooltip } from 'chart.js';
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Routes>
+
+          <Route path="/" element={<Login />} />  {/* Default route for login */}
+          <Route path="/home" element={<Home />} />  {/* Route for the home page */}
+          <Route path="/topics" element={<Topics />} />
+          <Route path="/register" element={<Register />} />
+
+          
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
